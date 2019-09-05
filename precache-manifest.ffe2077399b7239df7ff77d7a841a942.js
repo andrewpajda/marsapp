b260179c83296499e8290ec7793e29a165df823e
andrewpajda <andrewpajda@gmail.com>
2019-09-05 08:36:11 +0200

    Updates

---
+++ precache-manifest.ffe2077399b7239df7ff77d7a841a942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eead10028580ac434701457b553bacc2",
+    "url": "/marsapp/index.html"
+  },
+  {
+    "revision": "7dc02d19ff5b0325a883",
+    "url": "/marsapp/static/css/main.21af302a.chunk.css"
+  },
+  {
+    "revision": "72311a99d506edf75978",
+    "url": "/marsapp/static/js/2.f06ccd68.chunk.js"
+  },
+  {
+    "revision": "7dc02d19ff5b0325a883",
+    "url": "/marsapp/static/js/main.862efaf0.chunk.js"
+  },
+  {
+    "revision": "1c7aae73549114b55469",
+    "url": "/marsapp/static/js/runtime~main.a2220441.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-const rovers = ['Curiosity', 'Opportunity', 'Spirit'];
-
-ReactDOM.render(<App rovers={rovers} />, document.getElementById('root'));
